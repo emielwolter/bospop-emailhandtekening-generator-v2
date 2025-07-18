96d08627bdad94c5ba20416f58672546d2e9174e
emielwolter <emielwolter@icloud.com>
2025-07-18 18:09:55 +0200

    signature template changes

--- js/main.js
+++ js/main.js
@@ -15,16 +15,21 @@ function renderSignature(data) {
     ? `<a href="mailto:${data.email}">${data.email}</a>`
     : '<span class="placeholder">v.achternaam@bospopfestival.nl</span>';
   return `
-    <table class="sig-table" cellpadding="0" cellspacing="0">
-      <tr>
-        <td>
-          <strong>${name}</strong><br/>
-          ${role}<br/>
-          ${department}<br/>
-          ${phone}<br/>
-          ${email}
-        </td>
-      </tr>
+    <table cellspacing="0" cellpadding="0" border="0" style="COLOR: #262626; FONT-FAMILY: Arial, sans-serif; width:600px;">      
+        <tbody>
+            <tr>
+                <td style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; WIDTH:300px; COLOR: #000; line-height: 18px; border-right: solid 1px #000;">
+                    <span style="FONT-SIZE: 23pt; FONT-FAMILY: Arial Black, Arial, sans-serif; COLOR: #000; text-transform: uppercase;">${name}</span><br/>
+                    <span style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; COLOR: #000; font-weight: bold">${department}</span><br/>
+                    <span style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; COLOR: #000; font-weight: bold">${role}</span>
+                </td>
+                <td style="FONT-SIZE: 10pt; width: 300px; line-height: 18px; padding-left: 10px;">
+                    <span style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; COLOR: #000; font-weight: bold;">${phone}</span><br/>
+                    <span style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; COLOR: #000; font-weight: bold;">${email}</span><br/>
+                    <span style="FONT-SIZE: 10pt; FONT-FAMILY: Arial, sans-serif; COLOR: #000; font-weight: bold;">bospopfestival.nl</span><br/>
+                </td>
+            </tr>
+        </tbody>
     </table>
   `;
 }
